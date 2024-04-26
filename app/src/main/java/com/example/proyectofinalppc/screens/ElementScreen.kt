6378686126b04c6ee85593703dddaa46af2f9d38
Jimmy Saavedra <jim312002@hotmail.com>
2024-04-26 13:17:30 -0500

    implementacion de gridElement y searchBar modificada con el grid

--- app/src/main/java/com/example/proyectofinalppc/screens/ElementScreen.kt
+++ app/src/main/java/com/example/proyectofinalppc/screens/ElementScreen.kt
@@ -1,17 +1,28 @@
 package com.example.proyectofinalppc.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
@@ -22,8 +33,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.proyectofinalppc.R
 import com.example.proyectofinalppc.navigation.AppScreens
 
 @Composable
@@ -38,6 +54,7 @@ fun ElementScreen(navController: NavController) {
 fun ElementBodyContent(navController: NavController) {
     var query by remember { mutableStateOf("") }
     var active by remember { mutableStateOf(false) }
+    var showGrid by remember { mutableStateOf(true) }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
@@ -49,53 +66,133 @@ fun ElementBodyContent(navController: NavController) {
                 query = it
             },
             onSearch = {
-                active = false
+                showGrid = false
             },
             active = active,
             onActiveChange = {
                 active = it
+                showGrid = false
             },
             placeholder = { Text(text = "Search a country") },
             leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
         )
         {
             if (query.isNotEmpty()) {
-                val filteredCountries = countries.filter { it.contains(query, true) }
-                LazyColumn {
-                    items(filteredCountries) { country ->
-                        Text(text = country, modifier = Modifier
-                            .padding(8.dp)
-                            .clickable {
-                                active = false
-                            })
-                    }
-                }
+                val filteredElements = countries.filter { it.contains(query, true) }
+                ElementGridFiltered(
+                    navController = navController,
+                    filteredElements = filteredElements
+                )
             }
         }
     }
-    Column(
+    if (showGrid || query.isEmpty()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ElementGrid(navController)
+        }
+    }
+    Row(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Center
     ) {
-        Text("Element")
-        Button(onClick = {
-            navController.navigate(route = AppScreens.PlantDescription.route)
-        }) {
-            Text(text = "Plant description")
+        Button(
+            onClick = {
+                navController.navigate(route = AppScreens.MainMenu.route)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = androidx.compose.ui.graphics.Color.Transparent,
+                contentColor = androidx.compose.ui.graphics.Color.Black
+            ),
+            modifier = Modifier
+                .width(70.dp)
+                .height(70.dp)
+        ) {
+            Icon(painter = painterResource(R.drawable.back), contentDescription = "")
         }
-        Button(onClick = {
-            navController.navigate(route = AppScreens.FavoriteMenu.route)
-        }) {
-            Text(text = "Favorite Menu")
-        }
-        Button(onClick = {
-            navController.navigate(route = AppScreens.MainMenu.route)
-        }) {
-            Text(text = "Return")
+        Spacer(modifier = Modifier.width(200.dp))
+        Button(
+            onClick = {
+                navController.navigate(route = AppScreens.FavoriteMenu.route)
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = androidx.compose.ui.graphics.Color.Transparent,
+                contentColor = androidx.compose.ui.graphics.Color.Black
+            ),
+            modifier = Modifier
+                .width(70.dp)
+                .height(70.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Star, contentDescription = null)
         }
     }
 }
+
+@Composable
+fun ElementGrid(navController: NavController) {
+    val countries = countries
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 96.dp),
+        modifier = Modifier
+            .height(600.dp)
+            .padding(10.dp)
+            .border(1.dp, Color.Black)
+    ) {
+        items(countries) { country ->
+            ElementItem(onClick = {
+                navController.navigate(route = AppScreens.PlantDescription.route)
+            }, painterResource(id = R.drawable.ic_launcher_background), country)
+        }
+    }
+}
+
+@Composable
+fun ElementGridFiltered(navController: NavController, filteredElements: List<String>) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 96.dp),
+        modifier = Modifier
+            .height(600.dp)
+            .padding(10.dp)
+            .border(1.dp, Color.Black)
+    ) {
+        items(filteredElements) { country ->
+            ElementItem(onClick = {
+                navController.navigate(route = AppScreens.PlantDescription.route)
+            }, painterResource(id = R.drawable.ic_launcher_background), country)
+        }
+    }
+}
+
+@Composable
+fun ElementItem(
+    onClick: () -> Unit,
+    imagen: Painter,
+    nombre: String,
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = imagen,
+            contentDescription = null,
+            modifier = Modifier.size(80.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = nombre,
+            style = MaterialTheme.typography.titleSmall,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
 
 val countries = listOf(
     "Afghanistan",
